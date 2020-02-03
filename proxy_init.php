4cec18ed93b2529666f7f72f56f3f3047a5283c9
Cole Hudson <cole@colejhudson.com>
2020-02-02 16:20:42 -0800

    removed ^M and trailing spaces from all files

--- proxy_init.php
+++ proxy_init.php
@@ -1,1 +1,16 @@
-<?php	include('config.php');	include('curlEx.class.php');	include('database.class.php');		include('proxies/proxyNCSU.class.php');	$o = new proxyNCSU();	$db = new database();	$o->fill_db($db);	print_r($db->domain_check('sciencemag.org'));?>
+<?php
+
+	include('config.php');
+	include('curlEx.class.php');
+	include('database.class.php');
+	
+	include('proxies/proxyNCSU.class.php');
+
+	$o = new proxyNCSU();
+
+	$db = new database();
+	$o->fill_db($db);
+
+	print_r($db->domain_check('sciencemag.org'));
+
+?>
